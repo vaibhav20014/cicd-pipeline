38413e093a9e7c7a9e75e0e6368085c1a24c256c
himnishisrani <himnish54@gmail.com>
2024-09-10 13:54:46 +0530

    add conf

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+window.firebaseConfig = {
+    apiKey: "AIzaSyCfqoocGvPWyFANxaV0JBdCyaNkeXZY46s",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "devopsauth-ff33d",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "68062083313",
+    appId: "1:68062083313:web:7d4f34d585be2ca22ed150",
+    measurementId: "YOUR_MEASUREMENT_ID"
+};
