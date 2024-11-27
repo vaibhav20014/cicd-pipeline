ffdb68dbb6d8272734bbff6d4c312457bd463a8e
vaibhav20014 <102657020+vaibhav20014@users.noreply.github.com>
2024-11-27 12:55:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,4 +39,4 @@ app.post('/login', (req, res) => {
     });
 });
 
-app.listen(8001, () => console.log('Server running on port 8001'));
+app.listen(3000, () => console.log('Server running on port 3000'));
