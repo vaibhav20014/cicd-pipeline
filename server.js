9bded5d814aa2a35bb16fef4cfc28f81250d547e
vaibhav20014 <102657020+vaibhav20014@users.noreply.github.com>
2024-12-02 15:26:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use(express.json());
 
 // Create MySQL connection
 const dbConfig = {
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || '3306',
-    user: process.env.DB_USER || 'sa',
-    password: process.env.DB_PASSWORD || 'Admin@123',
-    database: process.env.DB_NAME || 'Testdb',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 };
 
 
