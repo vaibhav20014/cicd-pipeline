3e7de485435d65372f3df2326f496961e9e91096
vaibhav20014 <102657020+vaibhav20014@users.noreply.github.com>
2024-12-03 16:46:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mssql');
+const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const app = express();
